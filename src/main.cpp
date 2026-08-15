262143d4cc2d9d8e9032987bc61575076718f526
nixuntris <kyubi3kek@gmail.com>
2026-08-16 01:27:39 +0200

    Brickmap

--- src/main.cpp
+++ src/main.cpp
@@ -38,47 +38,98 @@ inline static Vector3 GetScreenToWorldRayOptimized(Vector2 position, Camera came
 
     return worldDir;
 }
-const int SIZE = 256;
-struct World {
-    uint8_t voxels[SIZE][SIZE][SIZE];
-    uint8_t accel[SIZE/4][SIZE/4][SIZE/4];
-    World() {
+const int SIZE = 1024;
+struct Chunk {
+    uint8_t *voxels;
+    bool containsBlocks;
+    uint8_t maxX;
+    uint8_t maxY;
+    uint8_t maxZ;
 
+    void Clear() {
+        containsBlocks = false;
+        for (int x = 0; x < 32; x++) {
+            for (int y= 0 ; y < 32; y++) {
+                for (int z = 0; z < 32; z++) {
+                    voxels[x*32*32+y*32+z] = 0;
+                }
+            }
+        }
+    }
+    void CalculateMaxSkipDistance() {
+        if (!containsBlocks) {
+            maxX = 15; //from the centre
+            maxY = 15; 
+            maxZ = 15; 
+        }
+    }
+};
+struct World {
+    Chunk chunks[SIZE/32][SIZE/32][SIZE/32];
+    uint8_t GetVoxel(int x, int y, int z) const {
+            
+        int cx = x / 32;
+        int cy = y / 32;
+        int cz = z / 32;
+        int lx = x % 32;
+        int ly = y % 32;
+        int lz = z % 32;
+        if (!chunks[cx][cy][cz].containsBlocks) return 0;
+        return chunks[cx][cy][cz].voxels[lx * 32 * 32 + ly * 32 + lz];
+    }
+
+    World() {
+        for (int x = 0; x < SIZE/32; x++) {
+            for (int y= 0 ; y < SIZE/32; y++) {
+                for (int z = 0; z < SIZE/32; z++) {
+                    chunks[x][y][z].voxels = (uint8_t*)MemAlloc(32*32*32);
+                    chunks[x][y][z].Clear();
+                }
+            }
+        }
         Image noise = GenImagePerlinNoise(
             SIZE,
             SIZE,
             0.0f, 
             0.0f, 
-            0.02f 
+            0.2f 
         );
 
-        Color *pixels = LoadImageColors(noise);
-
-        for (int x = 0; x < SIZE/4; x++) {
-            for (int z = 0; z < SIZE/4; z++) {
-                for (int y = 0; y < SIZE/4; y++) {
-                    accel[x][y][z] = 0;
-                }
-            }
-        }
+#pragma omp parallel for collapse(2)
         for (int x = 0; x < SIZE; x++) {
             for (int z = 0; z < SIZE; z++) {
 
+                
 
-                int height = GetRandomValue(45,50);
+                int height = GetImageColor(noise,x,z).r;
 
 
-                for (int y = 0; y < SIZE; y++) {
-                    accel[x/4][y/4][z/4] = 1;
-                    voxels[x][y][z] = (y<height)*GetRandomValue(1,3);
+                for (int y = 0; y <= height; y++) {
+                    if (height==y) {
+                        chunks[x/32][y/32][z/32].voxels[(x%32)*32*32+(y%32)*32+z%32] = GetRandomValue(1,2);
+                        chunks[x/32][y/32][z/32].containsBlocks = true;
+                    }
+                    else {
+                        chunks[x/32][y/32][z/32].containsBlocks = true;
+                        chunks[x/32][y/32][z/32].voxels[(x%32)*32*32+(y%32)*32+z%32] = 1;
+                    }
+                }
+            }
+        }
+        for (int x = 0; x < SIZE/32; x++) {
+            for (int y= 0 ; y < SIZE/32; y++) {
+                for (int z = 0; z < SIZE/32; z++) {
+                    if (!chunks[x][y][z].containsBlocks) {
+                        free(chunks[x][y][z].voxels);
+                    }
                 }
             }
         }
 
-        UnloadImageColors(pixels);
         UnloadImage(noise);
     }
 };
+Vector3 sunDirection = Vector3Normalize((Vector3){ 0.8f, 0.2f, 0.2f });
 class App {
     public:
     Camera camera;
@@ -91,7 +142,7 @@ class App {
     int *stepStorage;
     App() {
         InitWindow(width,height,"Voxelized");
-        camera.position = (Vector3){ SIZE/2, 50, SIZE/2 };
+        camera.position = (Vector3){ SIZE/2, 128, SIZE/2 };
         camera.target = (Vector3){ 0.0f, 2.0f, 0.0f };
         camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
         camera.fovy = 60.0f;
@@ -110,7 +161,7 @@ class App {
 
         const int lowWidth  = width / 4;
         const int lowHeight = height / 4;
-        const Color colors[10] = {SKYBLUE,WHITE,BLACK,GRAY};
+        const Color colors[10] = {SKYBLUE,GREEN,{uint8_t(GREEN.r*0.9),uint8_t(GREEN.g*0.9),uint8_t(GREEN.b*0.9),255},GRAY};
         while (!WindowShouldClose()) {
             BeginDrawing();
             ClearBackground(SKYBLUE);
@@ -120,6 +171,7 @@ class App {
 
             frame++;
 
+#pragma omp parallel for collapse(2)
             for (int x = 0; x < width; x++) {
                 for (int y = 0; y < height; y++) {
                     if ((x+y+frame)%2==0) continue; 
@@ -136,9 +188,11 @@ class App {
                 }
             }
 
+#pragma omp parallel for collapse(2)
             for (int x = 0; x < lowWidth; x++) {
                 for (int y = 0; y < lowHeight; y++) {
-
+                    
+                    if ((x+y+frame)%2==0) continue; 
                     int sampleX = x * 4 + 2;
                     int sampleY = y * 4 + 2;
 
@@ -155,11 +209,12 @@ class App {
                     int voxelZ = (int)floorf(start.z);
                     int stepX = (direction.x > 0.0f) -(direction.x < 0.0f);
                     int stepZ = (direction.z > 0.0f) - (direction.z < 0.0f);
+                    int stepY = (direction.y > 0.0f) - (direction.y < 0.0f);
                     float tDeltaX =(direction.x != 0.0f)? fabsf(1.0f / direction.x): INFINITY;
                     float tDeltaY = fabsf(1.0f / direction.y);
                     float tDeltaZ =(direction.z != 0.0f)? fabsf(1.0f / direction.z): INFINITY;
                     float tMaxX =(direction.x > 0.0f)? (((voxelX + 1) - start.x) / direction.x): (direction.x < 0.0f)? ((voxelX - start.x) / direction.x): INFINITY;
-                    float tMaxY =(voxelY - start.y) / direction.y;
+                    float tMaxY =(direction.y < 0.0f)? ((voxelY - start.y) / direction.y): ((voxelY + 1 - start.y) / direction.y);
                     float tMaxZ =(direction.z > 0.0f)? (((voxelZ + 1) - start.z) / direction.z): (direction.z < 0.0f)? ((voxelZ - start.z) / direction.z): INFINITY;
                     float previousT = 0.0f;
                     int steps = 0;
@@ -178,15 +233,19 @@ class App {
                             tMaxZ += tDeltaZ;
                         }
                         else {
-                            voxelY--;
+                            voxelY += stepY;
                             tMaxY += tDeltaY;
                         }
 
                         steps++;
-                        if (voxelX>=0 && voxelY>=0 && voxelZ>=0 && voxelX<SIZE && voxelY<SIZE && voxelZ<SIZE && world.voxels[voxelX][voxelY][voxelZ]!=0) break;
+                        if (voxelX>=0 && voxelY>=0 && voxelZ>=0 && voxelX<SIZE && voxelY<SIZE && voxelZ<SIZE){
+                            if (world.GetVoxel(voxelX,voxelY,voxelZ)!=0) break;
+                        }
+                        else {
+                            steps = 1000;
+                            break;
+                        }
                     }
-
-
                     float safeT =
                         fmaxf(0.0f, previousT - (float)safetyBacktrack);
 
@@ -203,7 +262,7 @@ class App {
                     stepStorage[x + y * lowWidth] = steps;
                 }
             }
-
+#pragma omp parallel for collapse(2)
             for (int x = 0; x < width; x++) {
                 for (int y = 0; y < height; y++) {
 
@@ -219,10 +278,6 @@ class App {
                     Vector3 direction =
                         directionStorage[x + y * width];
 
-                //    if (direction.y >= 0.0f)
-                 //       continue;
-
-
                     int lowX = x / 4;
                     int lowY = y / 4;
 
@@ -231,8 +286,12 @@ class App {
                             lowX + lowY * lowWidth
                         ];
 
-
-                    Vector3 rayStart = accelPos;
+                    const float boundaryEps = 1e-4f;
+                    Vector3 rayStart = {
+                        accelPos.x + direction.x * boundaryEps,
+                        accelPos.y + direction.y * boundaryEps,
+                        accelPos.z + direction.z * boundaryEps
+                    };
 
                     int voxelX = (int)floorf(rayStart.x);
                     int voxelY = (int)floorf(rayStart.y);
@@ -240,7 +299,7 @@ class App {
 
 
                     int stepX =(direction.x > 0.0f) -(direction.x < 0.0f);
-
+                    int stepY = (direction.y > 0.0f) - (direction.y < 0.0f);
                     int stepZ = (direction.z > 0.0f) -(direction.z < 0.0f);
 
 
@@ -265,29 +324,55 @@ class App {
 
                     while (steps < 1000) {
 
-                        if (voxelX>=0 && voxelY>=0 && voxelZ>=0 && voxelX<SIZE && voxelY<SIZE && voxelZ<SIZE && world.voxels[voxelX][voxelY][voxelZ]!=0) {
-                            
+                        if (voxelX>=0 && voxelY>=0 && voxelZ>=0 && voxelX<SIZE && voxelY<SIZE && voxelZ<SIZE) {
+                            if (world.GetVoxel(voxelX,voxelY,voxelZ)!=0) {
+                                                        
+                                Vector3 start = {
+                                    (float)voxelX + 0.5f + sunDirection.x * 0.1f,
+                                    (float)voxelY + 0.5f + sunDirection.y * 0.1f,
+                                    (float)voxelZ + 0.5f + sunDirection.z * 0.1f
+                                };
+                                float strength = 1.0;
+                                for (int k = 0; k < 128; k++) {
+                                    start.x += sunDirection.x;
+                                    start.y += sunDirection.y;
+                                    start.z += sunDirection.z;
+                                                                
+                                    if (start.x >= 0 &&  start.y >= 0 && start.z  >= 0 && 
+                                        start.x < SIZE &&  start.y < SIZE && start.z  < SIZE) {
+                                        if (world.GetVoxel((int)start.x,(int)start.y,(int)start.z) != 0) {
+                                            strength = 0.8f; // In shadow
+                                            break;
+                                        }
+                                    } else {
+                                        break; // Left the world, no shadow
+                                    }
+                                }
+                                uint8_t type = world.GetVoxel(voxelX,voxelY,voxelZ);
+                                ((unsigned char *)imageBuffer.data)[idx] = colors[type].r*strength;
+                                ((unsigned char *)imageBuffer.data)[idx + 1] = colors[type].g*strength;
+                                ((unsigned char *)imageBuffer.data)[idx + 2] =  colors[type].b*strength;
+                                break;
+                            }
+                                
+                            if (tMaxX < tMaxY && tMaxX < tMaxZ) {
+                                voxelX += stepX;
+                                tMaxX += tDeltaX;
+                            }
+                            else if (tMaxZ < tMaxY) {
+                                voxelZ += stepZ;
+                                tMaxZ += tDeltaZ;
+                            }
+                            else {
 
-                            ((unsigned char *)imageBuffer.data)[idx] = colors[world.voxels[voxelX][voxelY][voxelZ]].r;
-                            ((unsigned char *)imageBuffer.data)[idx + 1] = colors[world.voxels[voxelX][voxelY][voxelZ]].g;
-                            ((unsigned char *)imageBuffer.data)[idx + 2] =  colors[world.voxels[voxelX][voxelY][voxelZ]].b;
-                            break;
-                        }
+                                voxelY += stepY;
+                                tMaxY += tDeltaY;
+                            }
 
-                        if (tMaxX < tMaxY && tMaxX < tMaxZ) {
-                            voxelX += stepX;
-                            tMaxX += tDeltaX;
-                        }
-                        else if (tMaxZ < tMaxY) {
-                            voxelZ += stepZ;
-                            tMaxZ += tDeltaZ;
                         }
                         else {
-
-                            voxelY--;
-                            tMaxY += tDeltaY;
+                            break;
                         }
-
                         steps++;
                     }
                 }
